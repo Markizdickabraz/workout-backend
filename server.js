3e879f9fc3adb269ad37433eb29ca8112b7a5cf0
Markizdickabraz <mark.sahaxkiy@gmail.com>
2025-05-14 21:01:45 +0300

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
